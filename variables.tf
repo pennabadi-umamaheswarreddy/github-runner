7bebb87fda9f294c968487b7a378fc878e2bf7f9
Pennabadi Umamaheswar Reddy <Umamaheswar.Reddy@tecnotree.com>
2026-05-31 14:00:40 +0530

    github-runner

--- variables.tf
+++ variables.tf
@@ -8,12 +8,12 @@ variable "environment" {
 
 variable "zone_name" {
   type        = string
-  default     = "daws86s.fun"
+  default     = "umamaheswarreddy.fun"
   description = "description"
 }
 
 variable "zone_id" {
   type        = string
-  default     = "Z01894583A5LLRAMEST3A"
+  default     = "Z07585331TQLJV7FAYLUA"
   description = "description"
 }
